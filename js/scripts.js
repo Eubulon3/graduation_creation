a9008892d438d8129563f1a1e0cc0fc521f2ec52
chashu <chashu@chashus-MacBook-Pro.local>
2025-01-18 18:50:08 +0900

    1/18/18:49

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 const plt_img = document.getElementById("pltImg");
 const video = document.getElementById("simVideo");
 const video_sq = document.getElementById("simVideoSquare");
+const video_source = document.getElementById("simVideo-source")
+const video_sq_source = document.getElementById("simVideoSquare-source")
 const dark_overlay = document.getElementById("darkOverlay");
 const img_container = document.getElementById("imgContainer");
+const speed_buttons = document.querySelectorAll(".speed-controller div");
+const fixed_button = document.getElementById("fixed-button");
+const drl_button = document.getElementById("drl-button");
+
+fixed_button.addEventListener("click", (e) => {
+    changeVideo("../movs/fixed_edited.mov", fixed_button);
+})
+drl_button.addEventListener("click", (e) => {
+    changeVideo('../movs/ehira_a_total_edited.mov', drl_button);
+})
 
 img_container.addEventListener("click", (e) => {
     // 画像の位置とサイズを取得
@@ -28,3 +40,54 @@ video.addEventListener('timeupdate', function() {
     // 右側が暗くなるようにオーバーレイの幅を設定
     dark_overlay.style.width = `${100 - percentage}%`;
 });
+
+speed_buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        speed_buttons.forEach(button => button.classList.remove('selected'));
+        btn.classList.add("selected");
+
+        const speed = btn.getAttribute("data-speed");
+
+        video.playbackRate = parseFloat(speed);
+        video_sq.playbackRate = parseFloat(speed);
+    })
+});
+
+function togglePlayPause(){
+    const playIcon = document.getElementById("play").querySelector("img");
+    if(video.paused){
+        video.play();
+        video_sq.play();
+        playIcon.src = "../imgs/img_play.svg"
+    }else{
+        video.pause();
+        video_sq.pause();
+        playIcon.src = "../imgs/img_pause.svg"
+    }
+}
+
+function skipForward(){
+    video.currentTime -= 10;
+    video_sq.currentTime -= 10;
+}
+
+function skipLater(){
+    video.currentTime += 10;
+    video_sq.currentTime += 10;
+}
+
+function changeVideo(file_path, selectedButton){
+    video_source.src = file_path
+    video_sq_source.src = file_path
+    video.load();
+    video_sq.load();
+
+    fixed_button.classList.remove("selected");
+    drl_button.classList.remove("selected");
+    
+    // クリックされたボタンに.selectedクラスを追加
+    selectedButton.classList.add("selected");
+}
+
+
+
